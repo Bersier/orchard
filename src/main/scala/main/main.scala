ca88ddda32c27669b07d97eade75c2d2c697ade4
Stephane Bersier <stephane.bersier@gmail.com>
2023-02-07 14:07:27 -0500

    Looking decent

--- src/main/scala/main/main.scala
+++ src/main/scala/main/main.scala
@@ -3,4 +3,10 @@ package main
 @main
 def main(): Unit =
   import Orchard.*
-  println(first + one)
+  val a: Arr[Int] =
+    import scala.language.implicitConversions
+    Array(1, 2, 3)
+  println(a(first))
+  a(second) = 7
+  println(a(second))
+  println(after(after(first)).show)
--- src/main/scala/main/types.scala
+++ src/main/scala/main/types.scala
@@ -3,17 +3,42 @@ package main
 import scala.annotation.targetName
 
 object Orchard:
+  /** Type for ordinal numbers, used as indices. */
   opaque type Ordinal = Int
-  opaque type Cardinal = Int
 
-  // todo delete Cardinal altogether
-  given Conversion[Int, Cardinal] with
-    inline def apply(i: Int): Cardinal = i
-  given Conversion[Cardinal, Int] with
-    inline def apply(i: Cardinal): Int = i
+  /** Type for cardinal numbers, used as quantities. */
+  type Cardinal = Int
 
-  inline def cardinal(i: Int): Cardinal = i
+  /** Array type that only accepts ordinal indices. */
+  opaque type Arr[T] = Array[T]
+  extension [T](a: Arr[T])
+    inline def apply(i: Ordinal): T = a(i)
+    inline def update(i: Ordinal, newValue: T): Unit = a(i) = newValue
+    inline def length: Cardinal = a.length
+  given [T]: Conversion[Array[T], Arr[T]] with
+    inline def apply(a: Array[T]): Arr[T] = a
 
+  // Conversion functions between ordinals and cardinals
+  @targetName("beforeOrdinal") inline def before(i: Ordinal): Cardinal = i
+  @targetName("afterOrdinal") inline def after(i: Ordinal): Cardinal = i + 1
+  @targetName("beforeCardinal") inline def before(i: Cardinal): Ordinal = i - 1
+  @targetName("afterCardinal") inline def after(i: Cardinal): Ordinal = i
+
+  extension (i: Ordinal)
+    inline def + (j: Int): Ordinal = i + j
+    inline def - (j: Int): Ordinal = i - j
+    def show: String =
+      require(i >= 0)
+      val next = i + 1
+      next.toString + {
+        if next > 3 && next < 21 then "th" else next % 10 match
+          case 1 => "st"
+          case 2 => "nd"
+          case 3 => "rd"
+          case _ => "th"
+     }
+
+  // Convenience ordinal constants
   val first: Ordinal = 0
   val second: Ordinal = 1
   val third: Ordinal = 2
@@ -24,31 +49,4 @@ object Orchard:
   val eighth: Ordinal = 7
   val ninth: Ordinal = 8
   val tenth: Ordinal = 9
-
-  @targetName("beforeOrdinal") inline def before(i: Ordinal): Cardinal = i
-  @targetName("afterOrdinal") inline def after(i: Ordinal): Cardinal = i + 1
-  @targetName("beforeCardinal") inline def before(i: Cardinal): Ordinal = i - 1
-  @targetName("afterCardinal") inline def after(i: Cardinal): Ordinal = i
-
-  extension (i: 1)
-    inline def st: Ordinal = 0
-  extension (i: 2)
-    inline def nd: Ordinal = 1
-  extension (i: 3)
-    inline def rd: Ordinal = 2
-  extension (i: Int)
-    inline def th: Ordinal = i - 1
-
-  extension (i: Ordinal)
-    @targetName("ordinal+") inline def + (j: Cardinal): Ordinal = i + j
-    @targetName("ordinal-") inline def - (j: Cardinal): Ordinal = i - j
-
-  extension (i: Cardinal)
-    @targetName("cardinalAsInt") inline def asInt: Int = i
-    @targetName("cardinal+") inline def + (j: Cardinal): Cardinal = i + j
-    @targetName("cardinal-") inline def - (j: Cardinal): Cardinal = i - j
-    inline def * (j: Cardinal): Cardinal = i * j
-    inline def / (j: Cardinal): Cardinal = i / j
-    inline def % (j: Cardinal): Cardinal = i % j
-    inline def unary_- : Cardinal = -i
 end Orchard
